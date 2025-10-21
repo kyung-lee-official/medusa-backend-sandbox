6ee3a3957b1f0898eb02750a7546ef5cf55fdb2b
Kyung Lee <kyung.lee.official@outlook.com>
2025-10-21 18:20:28 +0800

    feat: add GET route for retrieving cart details by payment collection ID

---
+++ src/api/commerce-modules/payment/get-cart-by-payment-collection-id/[paymentCollectionId]/route.ts
@@ -0,0 +1,25 @@
+import { MedusaRequest, MedusaResponse } from "@medusajs/framework";
+import {
+	ContainerRegistrationKeys,
+	MedusaError,
+} from "@medusajs/framework/utils";
+
+export async function GET(req: MedusaRequest, res: MedusaResponse) {
+	const { paymentCollectionId } = req.params;
+	if (!paymentCollectionId) {
+		throw new MedusaError(
+			MedusaError.Types.INVALID_DATA,
+			"Payment collection ID is required"
+		);
+	}
+
+	const query = req.scope.resolve(ContainerRegistrationKeys.QUERY);
+	const { data: paymentCollections } = await query.graph({
+		entity: "payment_collection",
+		fields: ["cart.*"],
+		filters: {
+			id: paymentCollectionId,
+		},
+	});
+	return res.status(200).json(paymentCollections[0]);
+}
