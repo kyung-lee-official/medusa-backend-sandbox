781a31f615ea3d735dc637ff64b5343e774f3027
Kyung Lee <kyung.lee.official@outlook.com>
2025-10-28 16:38:24 +0800

    feat: expand fields in GET route for retrieving cart by payment collection ID

--- src/api/commerce-modules/cart/get-cart-by-payment-collection-id/[paymentCollectionId]/route.ts
+++ src/api/commerce-modules/cart/get-cart-by-payment-collection-id/[paymentCollectionId]/route.ts
@@ -16,10 +16,18 @@ export async function GET(req: MedusaRequest, res: MedusaResponse) {
 	const query = req.scope.resolve(ContainerRegistrationKeys.QUERY);
 	const { data: paymentCollections } = await query.graph({
 		entity: "payment_collection",
-		fields: ["cart.*", "cart.region.*", "cart.sales_channel.*"],
+		fields: [
+			"cart.*",
+			"cart.region.*",
+			"cart.customer.*",
+			"cart.sales_channel.*",
+			"cart.shipping_address.*",
+			"cart.shipping_methods.*",
+		],
 		filters: {
 			id: paymentCollectionId,
 		},
 	});
+
 	return res.status(200).json(paymentCollections[0]);
 }
