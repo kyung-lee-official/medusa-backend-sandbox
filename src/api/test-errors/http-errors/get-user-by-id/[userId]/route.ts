6afe9aca8a61ffbee72fcea0a32fe10a8c4bd43b
Kyung Lee <kyung.lee.official@outlook.com>
2025-12-03 21:04:08 +0800

    chore: ...

--- src/api/test-errors/http-errors/get-user-by-id/[userId]/route.ts
+++ src/api/test-errors/http-errors/get-user-by-id/[userId]/route.ts
@@ -12,7 +12,7 @@ export async function GET(req: MedusaRequest, res: MedusaResponse) {
 	const { userId } = req.params;
 	const user = mockDb.users.find((u) => u.id === Number(userId));
 	if (!user) {
-		throw new HttpError("AUTH.FORBIDDEN", "User not found", {
+		throw new HttpError("AUTH.FORBIDDEN", "Permission denied", {
 			foo: { userId },
 		});
 	}
